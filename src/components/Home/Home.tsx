437abe58990c87d60048afa23be21b718ea80c28
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-02-13 14:49:34 -0500

    Home is now guarded depending on auth

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {useContext} from 'react';
+import {UserContext} from 'components/Providers/UserProvider';
+import Header from 'components/Shared/Header';
+import Footer from 'components/Shared/Footer';
+
+import AuthHome from 'components/Home/AuthHome';
+import NoAuthHome from 'components/Home/NoAuthHome';
 
 const Home:React.FC = () => {
+
+    const user = useContext(UserContext);
     return(
-        <div>
-            Home!
-        </div>
+        <>
+            <Header />
+            {
+                user?
+                <AuthHome />
+                :
+                <NoAuthHome />
+            }
+            <Footer />
+        </>
     )
 }
 
