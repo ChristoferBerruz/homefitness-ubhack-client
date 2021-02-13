329f2e4008c7d8df66d459e6eff3c4ba8bf7dc0e
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-02-13 14:49:02 -0500

    Login page.
    Inserts a new user if not found in database

--- src/components/Authentication/Login.tsx
+++ src/components/Authentication/Login.tsx
@@ -1,13 +1,61 @@
 import React from 'react';
-import {SignInWithGoogle} from 'firebaseinit';
+import {useContext} from 'react';
+import {SignInWithGoogle, firestore} from 'firebaseinit';
+import Header from 'components/Shared/Header';
+import Footer from 'components/Shared/Footer';
+import {UserContext} from 'components/Providers/UserProvider';
+import firebase from "firebase/app";
+
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+// Attempts to add a user record into the database if it does not exist
+function addUserRecord({displayName, email, uid}:firebase.User){
+    const userRef = firestore.collection('users').doc(uid);
+
+    userRef.get()
+    .then((snapshot) => {
+
+        if(!snapshot.exists){
+            userRef.set({
+                displayName,
+                email,
+                uid
+            });
+        }
+
+    }).catch(err => {
+        alert("Something went wrong.." + err);
+    });
+}
 
 const Login: React.FC = () => {
 
+    const user = useContext(UserContext);
     return(
-        <div>
-            Try login in!
-            <button onClick={SignInWithGoogle}> Login In </button>
-        </div>
+        <>
+            <Header />
+                <Container>
+                    <Row>
+                        <Col>
+                            {
+                                user?
+                                <div>
+                                    You are already logged in... {addUserRecord(user!)}
+                                </div>
+                                :
+                                <div>
+                                    <h2>Sign in to Home Fitness</h2>
+                                    <Button onClick={SignInWithGoogle}> Sign in with Google</Button>
+                                </div>
+                            }
+                        </Col>
+                    </Row>
+                </Container>
+            <Footer />
+        </>
     )
 }
 
