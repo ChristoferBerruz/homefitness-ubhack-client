95177f0992659ee11d2e765a3ac7f94f56ffc7b7
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-02-13 21:28:27 -0500

    Auth guards at routing level

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import {
   BrowserRouter, 
   Route, 
-  Switch
+  Switch,
+  Redirect
 } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
@@ -10,14 +11,13 @@ import './custom.scss';
 import Login from 'components/Authentication/Login';
 import Logout from 'components/Authentication/Logout';
 import Home from 'components/Home/Home';
-
+import Profile from 'components/Profile/Profile';
 import {useAuth} from 'hooks/auth';
 import {UserContext} from 'components/Providers/UserContext';
 
 function App() {
 
   const {initializing, user} = useAuth();
-
   if (initializing){
     return <div></div>
   }
@@ -29,8 +29,10 @@ function App() {
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/home" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/logout" component={Logout} />
+            {!user && <Route path="/login" component={Login} />}
+            {user && <Route path="/logout" component={Logout} />}
+            {user && <Route path="/profile" component={Profile} />}
+            <Route render={() => <Redirect to="/home" />}/>
           </Switch>
         </BrowserRouter>
       </div>
